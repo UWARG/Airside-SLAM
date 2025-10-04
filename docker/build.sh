75a0aa6527af6f6d50be7727746a59a952bfbb7d
honzikschenk <nojschenk@gmail.com>
2025-10-04 14:54:41 -0400

    Adds parameter support for lidar driver port and range

--- docker/build.sh
+++ docker/build.sh
@@ -2,5 +2,10 @@
 set -e
 
 IMAGE_NAME="ros-ws"
+LIDAR_PORT="${1:-/dev/ttyAMA0}"
+LIDAR_RANGE="${2:-320}"
 
-docker build -t $IMAGE_NAME -f docker/Dockerfile .
+docker build -t $IMAGE_NAME \
+    --build-arg LIDAR_PORT="$LIDAR_PORT" \
+    --build-arg LIDAR_RANGE="$LIDAR_RANGE" \
+    -f docker/Dockerfile .
