75dc4207640b4d174127d05036295c57ce2d696b
Adityya Kaushal <adityyakaushal@gmail.com>
2026-01-08 16:29:11 -0500

    fix: changed axis for angular velocity

--- viso_test/main.py
+++ viso_test/main.py
@@ -100,9 +100,9 @@ with dai.Pipeline() as p:
                 imuData = imuQueue.tryGet()
                 if imuData is not None:
                     gyro = imuData.packets[-1].gyroscope
-                    angular_vx = -gyro.z
-                    angular_vy = -gyro.x
-                    angular_vz = -gyro.y
+                    angular_vx = gyro.z
+                    angular_vy = gyro.x
+                    angular_vz = gyro.y
                 else:
                     angular_vx = 0.0
                     angular_vy = 0.0
