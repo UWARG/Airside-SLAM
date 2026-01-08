0093c22cdb7e52750a6e57f169edf0996d68a75d
Adityya Kaushal <adityyakaushal@gmail.com>
2026-01-08 15:24:55 -0500

    fix: scaled covariances down by 10

--- viso_test/drone_connection.py
+++ viso_test/drone_connection.py
@@ -42,20 +42,20 @@ class DroneConnection:
             angular_vy,         # angular_vy (float, rad/s)
             angular_vz,         # angular_vz (float, rad/s)
             # Start of pose covariance matrix
-            [0.001,  0.0,   0.0,   0.0,   0.0,   0.0] + \
-                   [0.001,  0.0,   0.0,   0.0,   0.0] + \
-                          [0.001,  0.0,   0.0,   0.0] + \
-                                 [0.0005, 0.0,   0.0] + \
-                                        [0.0005, 0.0] + \
-                                              [0.001],
+            [0.0001,  0.0,   0.0,   0.0,   0.0,   0.0] + \
+                   [0.0001,  0.0,   0.0,   0.0,   0.0] + \
+                          [0.0001,  0.0,   0.0,   0.0] + \
+                                 [0.00005, 0.0,   0.0] + \
+                                        [0.00005, 0.0] + \
+                                              [0.0001],
             # End of pose covariance matrix
             # Start of velocity covariance matrix
-            [0.004,  0.0,   0.0,   0.0,   0.0,   0.0] + \
-                   [0.004,  0.0,   0.0,   0.0,   0.0] + \
-                          [0.004,  0.0,   0.0,   0.0] + \
-                                  [0.002, 0.0,   0.0] + \
-                                         [0.002, 0.0] + \
-                                              [0.005],
+            [0.0004,  0.0,   0.0,   0.0,   0.0,   0.0] + \
+                   [0.0004,  0.0,   0.0,   0.0,   0.0] + \
+                          [0.0004,  0.0,   0.0,   0.0] + \
+                                  [0.0002, 0.0,   0.0] + \
+                                         [0.0002, 0.0] + \
+                                              [0.0005],
             # End of velocity covariance matrix
             0,                  # reset_counter (uint8_t, 0 for no reset)
             mavutil.mavlink.MAV_ESTIMATOR_TYPE_VIO # estimator type
